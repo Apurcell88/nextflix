6d182e153811259b1fed324c9d3b6b88347c7884
Adam Purcell <apurcell88@gmail.com>
2025-02-18 16:07:10 -0600

    Some dependencies set up. Nav set up. DB semi setup? Nav styled.

---
+++ src/components/Nav.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { revalidatePath } from "next/cache";
+import { useRouter } from "next/navigation";
+
+const Nav = () => {
+  const router = useRouter();
+
+  return (
+    <div className="flex justify-between relative z-10">
+      <div>
+        <button
+          className="border-none text-red-600 text-2xl font-bold uppercase p-5 tracking-tighter transform"
+          onClick={() => revalidatePath("/")}
+        >
+          Nextflix
+        </button>
+      </div>
+      <div>
+        <button
+          className="bg-red-700 text-white w-20 h-8 m-6 rounded text-sm font-semibold"
+          onClick={() => router.push("/login")}
+        >
+          Sign In
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
