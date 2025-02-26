80bcb7f339d3b0ad94d04eff3440a6b61d151c13
Adam Purcell <apurcell88@gmail.com>
2025-02-26 14:56:54 -0600

    Set up movie carousel and TrendingCard component. Styled.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,28 +2,26 @@
 
 import GetStartedForm from "@/components/GetStartedForm";
 import Nav from "@/components/Nav";
-// import TrendingCard from "@/components/TrendingCard";
-import // Carousel,
-// CarouselContent,
-// CarouselItem,
-// CarouselNext,
-// CarouselPrevious,
-"@/components/ui/carousel";
+import TrendingCard from "@/components/TrendingCard";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
 
 import { useState, useEffect } from "react";
 
 export default function Home() {
   // state
   const [trending, setTrending] = useState([]);
-
-  const apiKey = process.env.MOVIE_API_KEY;
+  const [popularity, setPopularity] = useState(1);
 
   // Get trending movies
   useEffect(() => {
     const getTrendingMovies = async () => {
       try {
         const res = await fetch(
-          `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`
+          `https://api.themoviedb.org/3/movie/popular?api_key=51199ad127900c0adc25977cf075af18&language=en-US&page=1`
         );
         const data = await res.json();
 
@@ -34,16 +32,16 @@ export default function Home() {
     };
 
     getTrendingMovies();
-  }, [apiKey]);
+  }, []);
   console.log(trending);
 
   return (
     <div className="">
-      <div className="relative h-screen bg-[url(../../public/nextflix.jpg)] bg-cover">
+      <div className="relative h-[100%] bg-[url(../../public/nextflix.jpg)] bg-cover">
         {" "}
         {/* Added relative here */}
         <Nav /> {/* Nav component remains on top */}
-        <div className="absolute inset-x-0 top-0 h-full bg-gradient-to-b from-black/60 to-black"></div>{" "}
+        <div className="absolute inset-x-0 top-0 h-[40%] bg-gradient-to-b from-black/60 to-black"></div>{" "}
         {/* The overlay */}
         <div>
           <div className="mt-20 px-8 relative text-white text-center">
@@ -59,21 +57,22 @@ export default function Home() {
             </p>
             <GetStartedForm />
 
-            {/* more than likely going to break */}
-            {/* <Carousel>
-              <CarouselContent>
-                {trending.map((media: string) => (
-                  <CarouselItem key="blah" className="basis-1/3">
-                    <TrendingCard image={media.image} />
-                  </CarouselItem>
-                ))}
-              </CarouselContent>
-            </Carousel> */}
-
-            {/* {trending.map((media) => (
-              <TrendingCard key={"blah"} image={media.image} />
+            {/* {trending.map((movie) => (
+              <TrendingCard key={movie.title} image={movie.poster_path} />
             ))} */}
           </div>
+        </div>
+        <div className="mt-12 bg-black text-white h-screen pt-4">
+          <h2 className="pl-6 font-semibold text-lg pb-4">Trending Now</h2>
+          <Carousel className="px-10">
+            <CarouselContent>
+              {trending.map((movie) => (
+                <CarouselItem className="basis-1/3">
+                  <TrendingCard key={movie.title} image={movie.poster_path} />
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+          </Carousel>
         </div>
       </div>
     </div>
--- src/components/TrendingCard.tsx
+++ src/components/TrendingCard.tsx
@@ -1,7 +1,22 @@
 "use client";
+import Image from "next/image";
 
-const TrendingCard = ({}) => {
-  return <div>TrendingCard</div>;
+interface TrendingCardProps {
+  image: string;
+}
+
+const TrendingCard: React.FC<TrendingCardProps> = ({ image }) => {
+  return (
+    <div>
+      <Image
+        src={`https://image.tmdb.org/t/p/w500/${image}`}
+        alt="movie poster"
+        width={120}
+        height={120}
+        className="rounded-md"
+      />
+    </div>
+  );
 };
 
 export default TrendingCard;
